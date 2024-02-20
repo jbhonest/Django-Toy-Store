49e17d634ef3f833f6b530bb60fe0b4ae2d7172f
jbhonest <jbhonest@live.com>
2024-02-21 00:39:25 +0330

    added cart admin

--- cart/admin.py
+++ cart/admin.py
@@ -3,5 +3,25 @@ from django.db.models import Count
 from .models import Cart, CartItem
 
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product',  'quantity')
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'item_row', 'created_at')
+    list_filter = ('user', )
+    inlines = (CartItemInline,)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(item_row=Count('cartitems'))
+
+    @admin.display(ordering='item_row')
+    def item_row(self, cart):
+        return cart.cartitems.count()
