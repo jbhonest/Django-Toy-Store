d8bbdb0f3574cb4a59426a147152f5ee6942888c
jbhonest <jbhonest@live.com>
2024-02-21 00:15:00 +0330

    added cart app

---
+++ cart/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from store.models import Product
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='cartitems')
+    quantity = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.product.name
