fcdb888677ccd9a9d575a1fd888469f6b5276f1a
jbhonest <jbhonest@live.com>
2024-02-23 02:06:16 +0330

    minor changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,22 +1,11 @@
-from django.urls import path, include
 from rest_framework import routers
 from .views import CategoryViewSet, PostViewSet, CommentViewSet, ImageViewSet
 
-category_router = routers.DefaultRouter()
-category_router.register('', CategoryViewSet)
+router = routers.DefaultRouter()
+router.register('categories', CategoryViewSet)
+router.register('posts', PostViewSet)
+router.register('comments', CommentViewSet)
+router.register('images', ImageViewSet)
 
-post_router = routers.DefaultRouter()
-post_router.register('', PostViewSet)
-
-comment_router = routers.DefaultRouter()
-comment_router.register('', CommentViewSet)
-
-image_router = routers.DefaultRouter()
-image_router.register('', ImageViewSet)
-
-urlpatterns = [
-    path('categories/', include(category_router.urls)),
-    path('posts/', include(post_router.urls)),
-    path('comments/', include(comment_router.urls)),
-    path('images/', include(image_router.urls)),
-]
+# URLConf
+urlpatterns = router.urls
--- store/urls.py
+++ store/urls.py
@@ -1,22 +1,11 @@
-from django.urls import path, include
 from rest_framework import routers
 from .views import CategoryViewSet, ProductViewSet, CommentViewSet, ImageViewSet
 
-category_router = routers.DefaultRouter()
-category_router.register('', CategoryViewSet)
+router = routers.DefaultRouter()
+router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
+router.register('comments', CommentViewSet)
+router.register('images', ImageViewSet)
 
-product_router = routers.DefaultRouter()
-product_router.register('', ProductViewSet)
-
-comment_router = routers.DefaultRouter()
-comment_router.register('', CommentViewSet)
-
-image_router = routers.DefaultRouter()
-image_router.register('', ImageViewSet)
-
-urlpatterns = [
-    path('categories/', include(category_router.urls)),
-    path('products/', include(product_router.urls)),
-    path('comments/', include(comment_router.urls)),
-    path('images/', include(image_router.urls)),
-]
+# URLConf
+urlpatterns = router.urls
