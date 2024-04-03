b8eebadc866a3ce29b103b2a8aecb0b9699b1781
jbhonest <jbhonest@live.com>
2024-04-03 21:06:07 +0330

    minor changes

--- toystore/sample_settings.py
+++ toystore/sample_settings.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
 
@@ -8,3 +11,12 @@ ALLOWED_HOSTS = []
 
 # Define admin URL
 ADMIN_URL = 'admin/'
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
